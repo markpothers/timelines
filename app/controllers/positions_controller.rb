f779684f80811cdd5ba66c4422ba253713df76b3
github username <github email address>
2019-03-26 12:24:14 -0500

    reset links and forms after merge mess up

--- app/controllers/positions_controller.rb
+++ app/controllers/positions_controller.rb
@@ -10,10 +10,13 @@ class PositionsController < ApplicationController
 
   def new
      @position = Position.new
+     @employers = Employer.all
+     @cities = City.all
+     @people = Person.all
   end
 
   def create
-     @position = Position.new
+    @position = Position.new(position_params)
       if @position.save
         redirect_to position_path(@position)
       else
@@ -23,6 +26,9 @@ class PositionsController < ApplicationController
 
   def edit
     @position = Position.find(params[:id])
+    @employers = Employer.all
+    @cities = City.all
+    @people = Person.all
   end
 
   def update
@@ -38,13 +44,17 @@ class PositionsController < ApplicationController
   def destroy
     @position = Position.find(params[:id])
     @position.destroy
-    redirect_to index
+    redirect_to positions_path
   end
 
   private
 
   def position_params
-    params.require(position).permit(:title, :start_date, :finish_date, :person_id, :employer_id, :city_id)
+    params.require(:position).permit(:title, :start_date, :finish_date, :person => [:name], :employer => [:name], :city => [:name])
   end
+
+  # def person_params
+  #   params.require(:position).require(:person).permit(:name)
+  # end
 
 end

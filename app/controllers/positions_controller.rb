ba7af212592701e5b7b509192480a254b52a4a0e
github username <github email address>
2019-03-26 10:22:17 -0500

    positions form in place but still issues with editing

--- app/controllers/positions_controller.rb
+++ app/controllers/positions_controller.rb
@@ -10,10 +10,13 @@ class PositionsController < ApplicationController
 
   def new
      @position = Position.new
+     @employers = Employer.all
+     @cities = City.all
+     @people = Person.all
   end
 
   def create
-     @position = Position.new
+    @position = Position.new(position_params)
       if @position.save
         redirect_to position_path(@position)
       else
@@ -23,6 +26,9 @@ class PositionsController < ApplicationController
 
   def edit
     @position = Position.find(params[:id])
+    @employers = Employer.all
+    @cities = City.all
+    @people = Person.all
   end
 
   def update
@@ -44,7 +50,7 @@ class PositionsController < ApplicationController
   private
 
   def position_params
-    params.require(position).permit(:title, :start_date, :finish_date, :person_id, :employer_id, :city_id)
+    params.require(:position).permit(:title, :start_date, :finish_date, :person, :employer, :city)
   end
 
 end
--- app/models/position.rb
+++ app/models/position.rb
@@ -2,4 +2,18 @@ class Position < ApplicationRecord
   belongs_to :city
   belongs_to :employer
   belongs_to :person
+
+  def person=(name)
+    self.person_id = Person.find_or_create_by(name: name).id
+
+  end
+
+  def employer=(name)
+    self.employer_id = Employer.find_or_create_by(name: name).id
+  end
+
+  def city=(name)
+    self.city_id = City.find_or_create_by(name: name).id
+  end
+
 end

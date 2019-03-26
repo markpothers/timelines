72c742a7333f490573e2f47569d90351e8ac4bb9
EliLauff <eli.lauffenburger@gmail.com>
2019-03-26 17:40:32 -0500

    fixed edit and create issue, fixed issue created by select_date

--- app/controllers/positions_controller.rb
+++ app/controllers/positions_controller.rb
@@ -9,24 +9,18 @@ class PositionsController < ApplicationController
   end
 
   def new
-<<<<<<< HEAD
       @position = Position.new
-=======
-     @position = Position.new
-     @employer = Employer.new
-     @position.employer = @employer
-     @person = Person.new
-     @position.person = @person
-     @city = City.new
-     @position.city = @city
-     @employers = Employer.all
-     @cities = City.all
-     @people = Person.all
->>>>>>> bb197d8f5b0bfb561a4a630e697e0f485c4c8a30
+      @employers = Employer.all
+      @cities = City.all
+      @people = Person.all
   end
 
   def create
       @position = Position.new
+      p_info = position_params(:title,:person,:employer,:city,"start_date(1i)","start_date(2i)","start_date(3i)","finish_date(1i)","finish_date(2i)","finish_date(3i)")
+      @position.start_date = Date.new(p_info["start_date(1i)"].to_i,p_info["start_date(2i)"].to_i,p_info["start_date(3i)"].to_i)
+      @position.finish_date = Date.new(p_info["finish_date(1i)"].to_i,p_info["finish_date(2i)"].to_i,p_info["finish_date(3i)"].to_i)
+      @position.update_attributes(title: p_info[:title], person: p_info[:person], employer: p_info[:employer], city: p_info[:city])
       if @position.save
         redirect_to position_path(@position)
       else
@@ -36,6 +30,9 @@ class PositionsController < ApplicationController
 
   def edit
     @position = Position.find(params[:id])
+    @employers = Employer.all
+    @cities = City.all
+    @people = Person.all
   end
 
   def update
@@ -55,13 +52,18 @@ class PositionsController < ApplicationController
 
   private
 
-  def position_params
-<<<<<<< HEAD
-    params.require(position).permit(:title, :start_date, :finish_date, :person_id, :employer_id, :city_id)
-=======
-    params.require(:position).permit(:title, :start_date, :finish_date, :person_attributes => [:name], :employer_attributes => [:name], :city_attributes => [:name])
-    # params.require(:position).permit(:title, :start_date, :finish_date, :person => [:name], :employer => [:name], :city => [:name])
->>>>>>> bb197d8f5b0bfb561a4a630e697e0f485c4c8a30
+  def position_params(*args)
+    params.require(:position).permit(*args)
   end
+
+  # def start_date_from_params
+  #   info = params.require(:position).permit("start_date(1i)", "start_date(2i)", "start_date(3i)")
+  #   return Date.new(info[:position]["start_date(1i)"].to_i,info[:position]["start_date(2i)"].to_i,info[:position]["start_date(3i)"].to_i)
+  # end
+
+  # def finish_date_from_params
+  #   info = params.require(:position).permit("finish_date(1i)", "finish_date(2i)", "finish_date(3i)")
+  #   return Date.new(info[:position]["finish_date(1i)"].to_i,info[:position]["finish_date(2i)"].to_i,info[:position]["finish_date(3i)"].to_i)
+  # end
 
 end
--- app/models/position.rb
+++ app/models/position.rb
@@ -3,20 +3,16 @@ class Position < ApplicationRecord
   belongs_to :employer
   belongs_to :person
 
-  accepts_nested_attributes_for :person
-  accepts_nested_attributes_for :employer
-  accepts_nested_attributes_for :city
+  def person=(name)
+    self.person_id = Person.find_or_create_by(name: name).id
+  end
 
-  # def person=(name)
-  #   self.person_id = Person.find_or_create_by(name).id
-  # end
+  def employer=(name)
+    self.employer_id = Employer.find_or_create_by(name: name).id
+  end
 
-  # def employer=(name)
-  #   self.employer_id = Employer.find_or_create_by(name).id
-  # end
-
-  # def city=(name)
-  #   self.city_id = City.find_or_create_by(name).id
-  # end
+  def city=(name)
+    self.city_id = City.find_or_create_by(name: name).id
+  end
 
 end

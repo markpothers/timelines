15f14100d61dfaa11c9c4e934701a3284ec0c480
github username <github email address>
2019-03-27 11:34:21 -0500

    co_residents started

--- app/models/city.rb
+++ app/models/city.rb
@@ -6,10 +6,6 @@ class City < ApplicationRecord
   has_many :events_as_destination, :class_name => "Event", :foreign_key => "destination_id"
   has_many :visitors_as_destination, :through => :events_as_destination, :source => :visitor
 
-  def residents
-
-  end
-
   def average_residency
     durations = self.positions.map do |position|
         position.finish_date - position.start_date
@@ -21,5 +17,41 @@ class City < ApplicationRecord
       end
       average_seconds
   end
+
+  def co_residents
+    coincidences = []
+      all_positions = self.positions
+      self.positions.each do |position|
+        all_other_positions = all_positions.select do |other_position|
+          position != other_position
+        end
+          all_other_positions.each do |remaining_position|
+            if remaining_position.start_date <= position.finish_date || remaining_position.finish_date >= position.start_date
+              coincidences << [position, remaining_position]
+            end
+          end
+      end
+    coincidences
+  end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 end
--- app/models/employer.rb
+++ app/models/employer.rb
@@ -2,4 +2,5 @@ class Employer < ApplicationRecord
   has_many :positions
   has_many :people, through: :positions
   has_many :cities, through: :positions
+
 end

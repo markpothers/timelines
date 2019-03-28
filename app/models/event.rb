b3557505b8c29fbf6a2c8796e97b7ef2a32a4683
github username <github email address>
2019-03-28 16:24:15 -0500

    events updated and tablularized

--- app/models/event.rb
+++ app/models/event.rb
@@ -2,6 +2,10 @@ class Event < ApplicationRecord
   belongs_to :visitor, class_name: "Person", :foreign_key => "visitor_id"
   belongs_to :destination, class_name: "City", :foreign_key => "destination_id"
 
+  accepts_nested_attributes_for :visitor
+  accepts_nested_attributes_for :destination
+
+
   def visitor=(name)
     self.visitor_id = Person.find_or_create_by(name: name).id
   end

a23caddfe99bf9cd752fd464a011cf6abac5dd7f
github username <github email address>
2019-03-28 16:35:57 -0500

    resending updates

--- app/models/person.rb
+++ app/models/person.rb
@@ -33,6 +33,66 @@ class Person < ApplicationRecord
     coincident_locations
   end
 
+  def parallel_positions
+
+    coincident_locations = [counterpart, city, overlapstart, overlapfinish]
+
+    coincident_locations
+  end
+  
+
+  def co_located
+    coincident_locations = []
+
+
+
+    self.sorted_positions.each do |my_position|
+      other_positions = Position.where("city_id = #{my_position.city.id}")-self.sorted_positions
+      other_positions.each do |position|
+        if my_position.city == position.city && my_position.start_date <= position.finish_date && my_position.finish_date >= position.start_date
+ byebug
+          find_overlap(my_position, position)
+          coincident_locations << [position, last_start, first_finish]
+        end
+      end
+    end
+    coincident_locations
+  end
+
+
+
+
+
+
+
+  def find_overlap(my_position, their_position)
+    if my_position.start_date >= their_position.start_date
+      last_start = my_position.start_date
+    else
+      last_start = position.start_date
+    end
+    if my_position.finish_date <= position.finish_date
+      first_finish = my_position.finish_date
+    else
+      first_finish = position.finish_date
+    end
+    overlap = [last_start, first_finish]
+  end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
   def timeline
     timeline = []
       self.positions.each do |p|

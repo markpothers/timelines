f56dbe972dab83eccf5a24b671d3c537694491b1
EliLauff <eli.lauffenburger@gmail.com>
2019-03-28 18:05:40 -0500

    continued changes to navbar and site formatting

--- config/routes.rb
+++ config/routes.rb
@@ -7,4 +7,5 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   get '/', to: 'cities#home'
+  get '/search', to: 'cities#search'
 end
